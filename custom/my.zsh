0106a87b291a67a69bfd96ccf61d04c4bdc3e4b8
Maonx <imaonx@gmail.com>
2015-12-21 16:43:34 +0800

    modify my.zsh

--- custom/my.zsh
+++ custom/my.zsh
@@ -3,22 +3,34 @@
 #
 # brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
 #
-fpath=(/usr/local/share/zsh-completions $fpath)
+
+export TERM=screen-256color
+export EDITOR=vim
+
+
 alias zshconfig="vim ~/.zshrc"
 alias zshreload="source ~/.zshrc"
-alias tbsdk="~/Documents/taobaoSDK/TAE_SDK/htdocs"
-alias snsdk="~/Documents/suningSDK/sn_sdk/htdocs"
-alias startTaobaoSDK="sh ~/Documents/taobaoSDK/TAE_SDK/bin/startServer.sh"
-alias startSuningSDK="sh ~/Documents/suningSDK/sn_sdk/startServer.sh"
+alias npm="npm --registry=https://registry.npm.taobao.org "
+alias gitci="git add -A && git commit -m "
 alias csswatch="sass --sourcemap=none --watch --no-cache "
 alias cssout="sass --sourcemap=none --no-cache "
 alias vi="vim"
 alias addwords="vim ~/.vim/bundle/vimim/plugin/vimim.wubijd.txt"
+alias mux="tmuxinator"
+
+
+# alias tbsdk="~/Documents/taobaoSDK/TAE_SDK/htdocs"
+# alias snsdk="~/Documents/suningSDK/sn_sdk/htdocs"
+if [[ "$(uname -o)" = "Cygwin" ]]; then
+    alias startSuningSDK="/home/maonx/snSDK/SN_SDK.exe"
+    alias startTaobaoSDK="/home/maonx/taobaoSDK/bin/TAE-SDK-Launcher.exe"
+else
+    alias startTaobaoSDK="sh ~/Documents/taobaoSDK/TAE_SDK/bin/startServer.sh"
+    alias startSuningSDK="sh ~/Documents/suningSDK/sn_sdk/startServer.sh"
+fi
 alias cleanZip="zip -r --exclude='*_MACOSX*' --exclude='*.sass-cache*' --exclude='*.DS_Store*' "
 alias playOnlineOrigin="you-get -p /opt/homebrew-cask/Caskroom/vlc/2.2.1/VLC.app/Contents/MacOS/VLC "
 alias playOnlineHD2="you-get --format=hd2 -p /opt/homebrew-cask/Caskroom/vlc/2.2.1/VLC.app/Contents/MacOS/VLC "
-alias npm="npm --registry=https://registry.npm.taobao.org "
-alias gitci="git add -A && git ci -m "
-plugins=(z git colored-man colorize github pip python brew osx zsh-syntax-highlighting vi-mode)
-source ~/.tmux/tmuxinator.zsh
-export TERM=screen-256color
+
+
+plugins=(z git colored-man colorize github pip python brew osx zsh-syntax-highlighting vi-mode zsh-completions tmuxinator)
