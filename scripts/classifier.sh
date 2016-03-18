9596a4569c26ef22d07413f093d1df7a94392dab
Maonx <imaonx@gmail.com>
2016-03-18 17:14:23 +0800

    modify sh

--- scripts/classifier.sh
+++ scripts/classifier.sh
@@ -4,5 +4,9 @@ FLODER=$1
 PARAM=$2
 if [[ -d "$1" ]]; then
   cd $1
-  classifier "$2"
+  if [[ -n "$2" ]]; then
+    classifier 
+  else
+    classifier "$2"
+  fi
 fi
--- yaourtInstall.sh
+++ yaourtInstall.sh
@@ -2,3 +2,8 @@
 
 # install cron
 yaourt -S --noconfirm --needed dcron
+
+# need this to convert multiple images to pdf
+yaourt -S --noconfirm --needed ghostscript
+# image tool
+yaourt -S --noconfirm --needed imagemagick
