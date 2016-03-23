d7b130d3af1803bb36e65bb6fceb05a79f562fc5
Maonx <imaonx@gmail.com>
2016-03-23 17:12:32 +0800

    modify brew

--- brewInstall.sh
+++ brewInstall.sh
@@ -1,5 +1,29 @@
-#!/bin/bash
+#!/bin/evn bash
 
+# Install command-line tools using Homebrew.
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade --all
+
+# Install neovim
 brew install neovim/neovim/neovim
-# install nevim python3 plugin
-sudo pip3 install neovim
+# Install nevim python3 plugin
+# sudo pip3 install neovim
+
+
+
+
+
+
+
+# Remove outdated versions from the cellar.
+brew cleanup
