c481facdc4cc113fdfb1f11e10786fac85023f81
Maonx <imaonx@gmail.com>
2016-03-24 21:48:44 +0800

    add install

--- brewInstall.sh
+++ brewInstall.sh
@@ -19,12 +19,14 @@ brew install neovim/neovim/neovim
 # Install nevim python3 plugin
 # sudo pip3 install neovim
 
+brew install wget
 brew install tmux
 brew install proxychains-ng
 brew install pyenv
 # brew install python3
 # pyenv install --list
-# pyenv install 3.5.1
+# add -k to set local install,save tgz in $PYENV_ROOT/sources
+# pyenv install 3.5.1 -k
 # pyenv global 3.5.1
 # pyenv rehash
 brew install tree
@@ -35,8 +37,9 @@ brew install brew-cask
 #Install Appliations by brew cask
 brew cask install qq
 brew cask install shadowsocksx
-
-
+brew cask install neteasemusic
+brew cask install cleanmymac
+brew cask install the-unarchiver
 
 
 
